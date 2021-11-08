cece7acee0a59afac8753b358f8bfa4c0ce48a15
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-11-08 16:00:32 +0530

    _

--- main-application/src/components/Header.js
+++ main-application/src/components/Header.js
@@ -5,7 +5,7 @@ const username = window.prompt('What\'s your Name?: ');
 const Header = () => {
     return (
         <header>
-            <h1>Hello,<br/> I'm Madhan</h1>
+            <h1>Hello,<br/> I'm Aadithya</h1>
             <h3>Hi, {username}.</h3>
             <a id="know-more" href="#about">Know more</a>
             <a id="github" target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr">Github</a>
