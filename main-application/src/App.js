4592a5a15b93276059d4677e44fbd171e048b14f
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-11-09 17:01:41 +0530

    Navbar added

--- main-application/src/App.js
+++ main-application/src/App.js
@@ -1,13 +1,14 @@
+import React from 'react';
+
+import './style.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Feedback from './components/Feedback';
 import Footer from './components/Footer';
-import './style.css';
 
 function App() {
-  
   return (
     <div className="App">
       <Navbar />
