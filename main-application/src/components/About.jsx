b72f83d8d58d7b4cd6ce9e23dc829a102e8fd523
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-11-10 16:22:15 +0530

    Color combinations and backgrounds changed

--- main-application/src/components/About.jsx
+++ main-application/src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
         <div id="about">
                 <div className="text">
                     <h1 data-aos="fade-up" data-aos-delay="50" data-aos-duration="1000" data-aos-once="true">Who I am.</h1>
-                    <img data-aos="fade-up" data-aos-delay="500" data-aos-duration="1000" data-aos-once="true" src={profile} className="img-fluid float-start" width="40%" alt="..."/>
+                    <img data-aos="fade-up" data-aos-delay="500" data-aos-duration="1000" data-aos-once="true" src={profile} className="img-fluid float-lg-start" width="40%" alt="..."/>
                     <p data-aos="fade-up" data-aos-delay="250" data-aos-duration="1000" data-aos-once="true">
                         I'm Madhan Aadithya, I'm a Front-end Developer and a Pythonista I'm studying 8th Grade and started programming at 11 years old,
                         My first language is Python and I made more practice on it and atlast I started using Flask, But I need Html to create websites with Flask so I learnt Html very well and
--- main-application/src/components/Header.jsx
+++ main-application/src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../images/sideimg.png';
 
 const Header = () => {
     return (
@@ -8,7 +7,6 @@ const Header = () => {
             <h3>Front end Developer</h3>
             <a id="know-more" href="#about">Know more</a>
             <a id="github" target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr">Github</a>
-            <img src={logo} alt=""></img>
             <p>Scroll down 	&#8594;</p>
         </header>
     )
