b1ca86845cee215a971eebf2df60ce2c788099be
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-11-10 12:26:37 +0530

    .js changed to .jsx

--- main-application/src/components/script.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight= "0";
-  }
