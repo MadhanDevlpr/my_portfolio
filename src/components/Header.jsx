e53fc37b990916a4ab27d9826f02575df2319562
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-11-23 17:46:40 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,13 @@ const Header = () => {
             <h3>Front end Developer</h3>
             <a id="know-more" href="#about">Know more</a>
             <a id="github" target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr">Github</a>
+            <div className="terminal">
+                <div className="innertext">
+                    <span><p class="dollar">$ &nbsp;</p><p> Feel free to contribute to this on <a target="_blank" href="https://github.com/MadhanDevlpr/React.js-Portfolio">github</a>,</p></span>
+                    <span><p class="dollar">$ &nbsp;</p><p> I've made this portfolio with Reactjs and Css,</p></span>
+                    <span><p class="dollar">$ &nbsp;</p><p> <a target="_blank" href="https://madhandevlpr.github.io/React.js-Portfolio/">Documentation</a></p></span>
+                </div>
+            </div>
         </header>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,63 +12,63 @@ function Projects() {
                         <div className="card-body">
                         <h5 className="card-title">Weather App</h5>
                         <p className="card-text">This is a site made with html, css & js which simply shows the weather of any place accurately.</p>
-                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/weatherapp-js"><button type="button" className="btn btn-outline-light">Github</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/weatherapp-js"><button type="button" className="btn btn-primary">Github</button></a>
                         </div>
                     </div>
                     <div   className="card" >
                         <div className="card-body">
                         <h5 className="card-title">Javascript Clock</h5>
                         <p className="card-text">This is a very common project made with javascript but I've made some awesome stylings and animations.</p>
-                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/Js-Clock"><button type="button" className="btn btn-outline-light">Github</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/Js-Clock"><button type="button" className="btn btn-primary">Github</button></a>
                         </div>
                     </div>
                     <div className="card" >
                         <div className="card-body">
                         <h5 className="card-title">Fabulousfolio</h5>
                         <p className="card-text">This is a open source project which can be cloned and edited as per your wish to upgrade your old portfolios and make it fabulous.</p>
-                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/fabulousfolio"><button type="button" className="btn btn-outline-light">Github</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/fabulousfolio"><button type="button" className="btn btn-primary">Github</button></a>
                         </div>
                     </div>
                     <div  className="card" >
                         <div className="card-body">
                         <h5 className="card-title">Snake Game</h5>
                         <p className="card-text">I've made this with tkinter and python just for learning more complex tuples and lists so that it may help in more in scrapping.</p>
-                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/snake-game"><button type="button" className="btn btn-outline-light">Github</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/snake-game"><button type="button" className="btn btn-primary">Github</button></a>
                         </div>
                     </div>
                     <div className="card" >
                         <div className="card-body">
                         <h5 className="card-title">Terminal Quiz</h5>
                         <p className="card-text">This project was made when I started learning python's basics, this project will run all through the terminal.</p>
-                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/GK-QUIZ"><button type="button" className="btn btn-outline-light">Github</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/GK-QUIZ"><button type="button" className="btn btn-primary">Github</button></a>
                         </div>
                     </div>
                     <div  className="card" >
                         <div className="card-body">
                         <h5 className="card-title">Jarvis</h5>
                         <p className="card-text">I've made this project when I was good in the basics and this doesn't have neural networks, this is just a model of Artificial Intelligence.</p>
-                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/Jarvis"><button type="button" className="btn btn-outline-light">Github</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/Jarvis"><button type="button" className="btn btn-primary">Github</button></a>
                         </div>
                     </div>
                     <div className="card" >
                         <div className="card-body">
                         <h5 className="card-title">Background Generator</h5>
                         <p className="card-text">You can get the linear gradient background code s in this project and this is useful for Front-end Developers.</p>
-                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/Background-Generator"><button type="button" className="btn btn-outline-light">Github</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/Background-Generator"><button type="button" className="btn btn-primary">Github</button></a>
                         </div>
                     </div>
                     <div  className="card" >
                         <div className="card-body">
                         <h5 className="card-title">GUI Clock</h5>
                         <p className="card-text">This project was made just to practice my tkinter skills in python, and the main point is it will show the time.</p>
-                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/GUI-CLOCK"><button type="button" className="btn btn-outline-light">Github</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/GUI-CLOCK"><button type="button" className="btn btn-primary">Github</button></a>
                         </div>
                     </div>
                     <div className="card" >
                         <div className="card-body">
                         <h5 className="card-title">Text to Speech</h5>
                         <p className="card-text">There have been lots of Graphical text to speech programs, but this is based on the terminal.</p>
-                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/tts"><button type="button" className="btn btn-outline-light">Github</button></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/MadhanDevlpr/tts"><button type="button" className="btn btn-primary">Github</button></a>
                         </div>
                     </div>
                 </div>
