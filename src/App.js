a28cc9d851cee513502d88c505dd1835be4e1535
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-12-02 17:05:35 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,49 +5,49 @@ import Loader from './components/Loader';
 const Navbar = lazy(()=>{
   return Promise.all([
     import("./components/Navbar"),
-    new Promise(resolve => setTimeout(resolve, 8000))
+    new Promise(resolve => setTimeout(resolve, 2000))
   ])
   .then(([moduleExports]) => moduleExports);
 });
 const Header = lazy(() => {
   return Promise.all([
     import("./components/Header"),
-    new Promise(resolve => setTimeout(resolve, 8000))
+    new Promise(resolve => setTimeout(resolve, 2000))
   ])
   .then(([moduleExports]) => moduleExports);
 });
 const Projects = lazy(()=>{
   return Promise.all([
     import("./components/Projects"),
-    new Promise(resolve => setTimeout(resolve, 8000))
+    new Promise(resolve => setTimeout(resolve, 2000))
   ])
   .then(([moduleExports]) => moduleExports);
 });
 const About = lazy(()=>{
   return Promise.all([
     import("./components/About"),
-    new Promise(resolve => setTimeout(resolve, 8000))
+    new Promise(resolve => setTimeout(resolve, 2000))
   ])
   .then(([moduleExports]) => moduleExports);
 });
 const Skills = lazy(()=>{
   return Promise.all([
     import("./components/Skills"),
-    new Promise(resolve => setTimeout(resolve, 8000))
+    new Promise(resolve => setTimeout(resolve, 2000))
   ])
   .then(([moduleExports]) => moduleExports);
 });
 const Feedback = lazy(()=>{
   return Promise.all([
     import("./components/Feedback"),
-    new Promise(resolve => setTimeout(resolve, 8000))
+    new Promise(resolve => setTimeout(resolve, 2000))
   ])
   .then(([moduleExports]) => moduleExports);
 });
 const Footer = lazy(()=>{
   return Promise.all([
     import("./components/Footer"),
-    new Promise(resolve => setTimeout(resolve, 8000))
+    new Promise(resolve => setTimeout(resolve, 2000))
   ])
   .then(([moduleExports]) => moduleExports);
 });
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,28 @@
 import React from 'react';
 function Navbar() {
-  const navtoggle = () =>{
-    if(window.innerWidth >= 992){
-      if (window.scrollY >= 20){
-        navShrink();
-      }
-      else{
-        navExpand();
-      }
-    }
-    else{
-      navShrink();
-    }
-  }
-  window.addEventListener('scroll',navtoggle);
-  window.addEventListener('resize',navtoggle);
-  window.addEventListener('load',navtoggle);
-  
-  const navShrink = () =>{  
-    document.getElementsByClassName('navbar')[0].classList.replace('p-4','p-1');
-    document.getElementsByClassName('navbar')[0].style.background = "rgba(0,0,0,0.9)";
-  }
-  const navExpand = () =>{
-    document.getElementsByClassName('navbar')[0].classList.replace('p-1','p-4');
-    document.getElementsByClassName('navbar')[0].style.background = "transparent";
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
   }
   
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
     return (
         <div>
-            <nav id="navbar navbar-nav" className="navbar navbar-expand-lg navbar-dark fixed-top p-4">
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                  <li className="nav-item active m-3">
-                    <a className="nav-link"  href="#header">Home <span className="sr-only">(current)</span></a>
-                  </li>
-                  <li className="nav-item active m-3">
-                    <a className="nav-link"  href="#projectss">Projects</a>
-                  </li>
-                  <li className="nav-item active m-3">
-                    <a className="nav-link"  href="#about">About</a>
-                  </li>
-                  <li className="nav-item active m-3">
-                    <a className="nav-link"  href="#feedback">Feedback</a>
-                  </li>
-                </ul>
+          <svg onClick={openNav} className="toggle-btn" viewBox="0 0 100 80" width="40" height="40">
+          <rect width="100" height="15"></rect>
+          <rect y="30" width="100" height="15"></rect>
+          <rect y="60" width="100" height="15"></rect>
+          </svg>
+            <div id="mySidenav" className="sidenav">
+              <a className="closebtn" onClick={closeNav}>×</a>
+              <div className="links">
+              <a onClick={closeNav} className="item" href="#header">home</a>
+              <a onClick={closeNav} className="item" href="#projectss">Projects</a>
+              <a onClick={closeNav} className="item" href="#about">About</a>
+              <a onClick={closeNav} className="item" href="#feedback">Feedback</a>
               </div>
-            </nav>
+            </div>
         </div>
     )
 }
