3660620a49bae0411e4f9170ad493b0dda6c1c16
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-12-02 21:06:46 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 function Navbar() {
   function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.top = "0%";
+    document.getElementById("mySidenav").style.background = "#3fd2c7";
   }
   
   function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.top = "-100%";
+    document.getElementById("mySidenav").style.background = "#00458b";
   }
     return (
         <div>
           <svg onClick={openNav} className="toggle-btn" viewBox="0 0 100 80" width="40" height="40">
-          <rect width="100" height="15"></rect>
-          <rect y="30" width="100" height="15"></rect>
-          <rect y="60" width="100" height="15"></rect>
+            <rect width="100" height="15"></rect>
+            <rect y="30" width="100" height="15"></rect>
+            <rect y="60" width="100" height="15"></rect>
           </svg>
             <div id="mySidenav" className="sidenav">
               <a className="closebtn" onClick={closeNav}>×</a>
