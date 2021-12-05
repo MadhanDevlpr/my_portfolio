2e40383153cb50974f0088b090f93925862eb35f
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-12-05 16:27:49 +0530

    blazing update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import video from '../images/video.mp4';
 
 const Header = () => {
     return (
         <header id="header">
+            <video src={video} autoPlay muted loop></video>
             <h1>HI THERE I'M ,<br/><span className="glow">aadithya</span></h1>
             <h3>DEVELOPER + DESIGNER</h3>
             <a id="know-more" href="#about">Know more</a>
