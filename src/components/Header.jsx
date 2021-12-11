de216889726f464a2b95acb2cceb0cf41e81d723
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-12-11 11:43:24 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import background from '../images/background.png';
 
 const Header = () => {
+    
     return (
         <header id="header">
             <h1>HI THERE I'M ,<br/><span className="glow">aadithya</span></h1>
