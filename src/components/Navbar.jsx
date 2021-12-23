4183bf27045dd600759561d0819bb2774a022722
MADHAN AADITHYA <pmaadithya09@gmail.com>
2021-12-23 17:26:22 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,13 +23,12 @@ function Navbar() {
     return (
         <div>
           <nav id="nav" className="navbar fixed-top p-4 navbar-expand-lg navbar-dark">
-              <a className="navbar-brand p-4" style={{color:"#00ffff",fontSize:"22px"}} href="#header">m.</a>
+              <a className="navbar-brand p-4" style={{color:"#00ffff",fontSize:"22px",fontWeight:"900"}} href="#header">m.</a>
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
-
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mx-auto">
+                <ul className="navbar-nav ml-auto">
                   <li className='nav-object'>
                     <a className="item" href="#header">home <span className="sr-only">(current)</span></a>
                   </li>
