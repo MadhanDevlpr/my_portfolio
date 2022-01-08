b203ed2daec4c9b02aa346860dfaf73796f2be97
MADHAN AADITHYA <pmaadithya09@gmail.com>
2022-01-08 17:02:47 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,9 @@
 import React from 'react';
 const Header = () =>{
-    const scrollAnime = ()=>{
-        if ((window.innerWidth >= 900)){
-            if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                document.getElementById('header').style.transform = 'perspective(1000px) scale(0.9) rotateX(5deg)';
-                if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-                    document.getElementById('header').style.transform = 'perspective(1000px) scale(0.85) rotateX(12.5deg)';
-                }
-                if(document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-                    document.getElementById('header').style.transform = 'perspective(1000px) scale(0.8) rotateX(12.5deg)';
-                }
-                if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-                    document.getElementById('header').style.transform = 'perspective(1000px) scale(0.75) rotateX(25deg)';
-                }
-                if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-                    document.getElementById('header').style.transform = 'perspective(1000px) scale(0.7) rotateX(25deg)';
-                }
-            }
-            else{
-                document.getElementById('header').style.transform = 'perspective(1000px) scale(1) rotateX(0deg)';
-            }
-    }
-    };
-    window.onscroll = scrollAnime
     return (
         <header id="header">
-            <h1>HI THERE I'M ,<br/><span className="glow">aadithya</span></h1>
+            <h1>making my dreams come true by<br/><span className="glow">developing</span></h1>
             <h3>I build highly interactive sites with pure css and javascript, the languages well known for me are <code>Python, Html, Css, Sass, Javacript</code>, the tools and frameworks I use are <code>Vs Code, React, Flask, Git Bash, WSL.</code></h3>
-            <a id="know-more" href="#about">know more</a>
-            <a id="projects-btn" href="#projectss">projects</a>
         </header>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
         document.getElementById('nav').style.opacity = "1";
       }
       else{
-        document.getElementById('nav').style.opacity = "0";
+        document.getElementById('nav').style.opacity = "1";
       }
     }
     else{
@@ -20,7 +20,7 @@ function Navbar() {
     return (
         <div>
           <nav id="nav" className="navbar fixed-top p-0 navbar-expand-lg navbar-light">
-              <a className="navbar-brand p-4" style={{color:"#20A9E8",fontSize:"22px",fontWeight:"450"}} href="#header">m.</a>
+              <a className="navbar-brand p-4" style={{color:"#909090",fontSize:"22px",fontWeight:"450"}} href="#header">m.</a>
               <button className="navbar-toggler m-4 p-1" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <svg className="toggler" xmlns="http://www.w3.org/2000/svg" fill='#fff' width="30" height="30" className="bi bi-list" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
