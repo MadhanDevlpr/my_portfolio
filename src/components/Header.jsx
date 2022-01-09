d3177445570a3929bfaf27233e4848eaabee3902
MADHAN AADITHYA <pmaadithya09@gmail.com>
2022-01-09 13:22:27 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () =>{
     return (
         <header id="header">
-            <h1>making my dreams come true by<br/><span className="glow">developing</span></h1>
+            <h1>making my dreams become true by<br/><span className="glow">developing</span></h1>
             <h3>I build highly interactive sites with pure css and javascript, the languages well known for me are <code>Python, Html, Css, Sass, Javacript</code>, the tools and frameworks I use are <code>Vs Code, React, Flask, Git Bash, WSL.</code></h3>
         </header>
     )
