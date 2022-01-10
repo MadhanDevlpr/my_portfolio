97fb1ee72161befe54ab930388b0f7b8d2f5cfc0
MADHAN AADITHYA <pmaadithya09@gmail.com>
2022-01-10 20:32:37 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ function Navbar() {
               <img src={logo} width="30" height="30" alt=""/>
               </a>
               <button className="navbar-toggler m-4 p-1" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <svg className="toggler" xmlns="http://www.w3.org/2000/svg" fill='#fff' width="30" height="30" className="bi bi-list" viewBox="0 0 16 16">
+              <svg className="toggler bi bi-list" xmlns="http://www.w3.org/2000/svg" fill='#fff' width="30" height="30" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
               </svg>
               </button>
