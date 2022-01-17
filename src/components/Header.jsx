4fcb4016a17f54a1501fbcc133291d2114b11eea
Madhan Aadithya <pmaadithya09@gmail.com>
2022-01-17 11:05:00 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,127 @@ import React from "react";
 const Header = () =>{
     return (
         <section id="header">
+            // Content
             <h1>making my dreams become true by<br/><span className="glow">developing</span></h1>
-            <h3>I build highly interactive sites with pure css and javascript, the languages well known for me are <code>Python, Html, Css, Sass, Javacript</code>, the tools and frameworks I use are <code>Vs Code, React, Flask, Git Bash, WSL.</code></h3>
-            
+            <h3>I build highly interactive sites
+             with pure css and javascript,
+            the languages well known for me are
+            <code>Python, Html, Css, Sass, Javacript</code>, the tools and frameworks I use are
+            <code>Vs Code, React, Flask, Git Bash, WSL.</code></h3>
+
+            // animation
+            <div id="anime">
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+
+
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+
+
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+
+
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+            </div>
         </section>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
               <a className="p-4" href="#header">
                 <img src={logo} width="20" height="20" alt=""/>
               </a>
-              
+
               <button onClick={closeNav} id="closeNav" className="navbar-toggler closenav m-4 p-1" type="button">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#21212b" class="bi bi-x" viewBox="0 0 16 16">
                   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
@@ -36,7 +36,7 @@ function Navbar() {
             </div>
             <div id="sidenav" className="sidenav-m">
                 <ul>
-                  
+
                   <li>
                     <a className="navbar-item p-2" onClick={closeNav} href="#header">home <span className="sr-only">(current)</span></a>
                   </li>
@@ -53,7 +53,6 @@ function Navbar() {
                     <a className="navbar-item p-2" onClick={closeNav} href="#feedback">feedback</a>
                   </li>
                 </ul>
-                <a id="news" href="http://eepurl.com/hCZPS1" target="_blank">Subscribe to my NewsLetter</a>
               </div>
         </div>
     )
