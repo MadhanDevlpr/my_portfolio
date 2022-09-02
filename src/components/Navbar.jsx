1e7098c411bda6dad2f0d749883cc18274ff6394
Madhan Aadithya <pmaadithya09@gmail.com>
2022-09-02 21:26:04 +0530

    color palette update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,12 +24,12 @@ function Navbar() {
               </a>
 
               <button onClick={closeNav} id="closeNav" className="navbar-toggler closenav m-4 p-1" type="button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#21212b" className="bi bi-x" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fcd3bd" className="bi bi-x" viewBox="0 0 16 16">
                   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                 </svg>
               </button>
               <button onClick={openNav} id="openNav" className="navbar-toggler m-4 p-1" type="button">
-                <svg className="toggler bi bi-list" xmlns="http://www.w3.org/2000/svg" fill="#21212b" width="20" height="20" viewBox="0 0 16 16">
+                <svg className="toggler bi bi-list" xmlns="http://www.w3.org/2000/svg" fill="#fcd3bd" width="20" height="20" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                 </svg>
               </button>
