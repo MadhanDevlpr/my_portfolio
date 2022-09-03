91711d567ad38dc961f74bbc8250badd074cced0
Madhan Aadithya <pmaadithya09@gmail.com>
2022-09-03 17:49:07 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,8 +7,9 @@ function About() {
                 <div className="text">
                     <h1>Who I am.</h1>
                     <p>
-                        I'm Madhan Aadithya, I'm a Front-end Developer and a Pythonista I'm studying 9th Grade and I started programming at 11 years old,
-                        I started my journey with Python and made more practice on it and atlast I started using a web framework called Flask, I learnt html to make static websites with Flask but just Html was not so good so
+                        I'm Madhan Aadithya, a Front-end Developer and a Pythonista, studying in 9th Grade,
+                        I started my programming journey with Python and made numerous projects (see the few in the next section).
+                        And atlast I started using a web framework called Flask to develop websites, the next stepof me was to learn HTML and CSS, I succ
                         I learnt Css for styling the Html but I was'nt satisfied with my work, I just created Static websites but now in the recent past I've learnt javascript and learnt to create Dynamic websites.
                         I've also learnt Bootstrap for making some lightweight projects.
                     </p>
