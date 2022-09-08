4fb1aee5d3b8e2bbd183d6e1e5e15308d2deef14
Madhan Aadithya <pmaadithya09@gmail.com>
2022-09-08 11:37:27 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,8 @@ const Header = () =>{
                 <h3>I build highly interactive sites
                 with pure css and javascript,
                 the languages well known for me are
-                <code>Python, Html, Css, Sass, Javacript</code>, the tools and frameworks I use are
-                <code>Vs Code, React, Flask, Git Bash, WSL.</code></h3>
+                <span>Python, Html, Css, Sass, Javacript</span>, the tools and frameworks I use are
+                <span>Vs Code, React, Flask, Git Bash, WSL.</span></h3>
             </div>
             <div className="circle-container"><div className="circle"></div></div>
             
