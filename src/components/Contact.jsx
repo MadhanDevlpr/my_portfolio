3b35d8266a9ed800af98d23c312f859e9dc641ab
Madhan Aadithya <pmaadithya09@gmail.com>
2022-09-09 21:05:51 +0530

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,16 +3,19 @@ import React from 'react'
 function Feedback() {
     return (
         <section className="feedbk" id="feedback">
-            <h1>Greetings!</h1><br />
-            <p>Always open for ideas.</p>
+            <h1>What's new?</h1><br />
+            <p>Tell me what you've got, to improve or to correct or to add or something else.</p>
             <form action="https://formsubmit.co/pmaadithya09@gmail.com" method="POST" acceptCharset="UTF-8"><br />
             <div className="wrap-input">
                 <input type="hidden" name="_next" value="https://madhanaadithya.studio"/>
-                <input type="hidden" name="_autoresponse" value="Hello, your message successfully reached!, forgot who is this? It's Madhan Aadithya."/>
-                <input required placeholder="Email" type="email" name="Email" /><br />
-                <input required placeholder="Name" type="text" name="Name" /><br /><br />
+                <div className="wrapper">
+                    <input required placeholder="Email" type="email" name="Email" /><br />
+                    <input required placeholder="Name" type="text" name="Name" /><br /><br />
+                    <input type="hidden" name="_autoresponse" value="Hello, your message successfully reached!, forgot who is this? It's Madhan Aadithya."/>
+                </div>
+                <textarea required className="feedf" placeholder="Message" type="text" name="Message" spellCheck="true"></textarea>
             </div><br /><br />
-            <textarea required className="feedf" placeholder="Feedback" type="text" name="Feedback" spellCheck="true" min-height="400px" width="auto"></textarea><br /><br />
+            <br /><br />
             <button type="submit" className="btn btn-light buttonn">Send</button><br /><br />
             </form>
         </section>
