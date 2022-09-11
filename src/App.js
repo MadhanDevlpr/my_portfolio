ac0c3e500093f98d74e6f6d6512311f333af0dae
Madhan Aadithya <pmaadithya09@gmail.com>
2022-09-11 12:55:20 +0530

    update

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Projects from './components/Projects';
 import About from './components/About';
-import Skills from './components/Skills';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
+
+import { BrowserRouter as Router,Route, Link, Routes} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-        <Loader />
-        <Navbar />
-        <Header />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-        <Footer />
-    </div>
+    <Router>
+      <Loader />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Header />}/>
+        <Route path="/about"  element={<About/>}/>
+        <Route path="/projects"  element={<Projects/>}/>
+        <Route path="/contact"  element={<Contact/>}/>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -8,12 +8,6 @@ const Loader = () => {
         <div id="preloader">
             <div id="loader" className="loader-wrapper">
                 <div id="name"><img src={name} alt="Madhan Aadithya" /></div>
-                <div>Html</div>
-                <div>Css</div>
-                <div>Sass</div>
-                <div>Javascript</div>
-                <div>React.js</div>
-                <div>Python</div>
                 <div>Welcome to my portfolio.</div>
             </div>
         </div>
